d5ba9c8cd5956011d381d921f77e6c5022ed487f
Murali C <haribabu29@outlook.com>
2020-12-30 23:43:31 -0800

    Pattern Match - Problem1.java

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Mastering in Java
 1. [SimpleHelloWorld](/src/SimpleHelloWorld.java)
+2. [Simple pattern match](/src/patternsearch/Problem1.java)
---
+++ src/patternsearch/Problem1.java
@@ -0,0 +1,21 @@
+package patternsearch;
+
+/**
+ * Pattern Search
+ */
+public class Problem1 {
+
+    public static void main(String[] args) {
+        Problem1.search("AABAACAADAABAABA" , "AABA");
+    }
+
+    public static void search( String text, String pattern ) {
+        for(int i = 0 ; i <= text.length() - pattern.length(); i++ ) {
+            if (text.charAt(i) == pattern.charAt(0)) {
+                if( text.substring(i, i + pattern.length()).equals(pattern)) {
+                    System.out.println("Pattern found at index" + i);
+                }
+            }
+        }
+    }
+}
